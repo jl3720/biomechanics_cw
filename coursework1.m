12a260b475bc1a113d046dc83d982a5ba9f2018b
James <jameszcliu@gmail.com>
2022-11-08 19:53:13 +0000

    lts_p_lkm = inv(t_mat80);# Please enter the commit message for your changes. Lines starting

--- coursework1.m
+++ coursework1.m
@@ -79,9 +79,49 @@ lgd.Location = 'southwest';
 % left_thigh_sup = left_thigh_sup(left_thigh_sup{:,'Frame'} == 98, :);
 % left_thigh_sup = left_thigh_sup{:,2:end}
 
-lts = data(:,{'Frame','L_Thigh_SuperiorX', 'L_Thigh_SuperiorY', 'L_Thigh_SuperiorZ', 'L_Thigh_InferiorX', 'L_Thigh_InferiorY', 'L_Thigh_InferiorZ', 'L_Thigh_LateralX', 'L_Thigh_LateralY', 'L_Thigh_LateralZ'})
-lts = lts{lts{:,'Frame'} == 98, :}
+lts0 = data(:,{'Frame','L_Thigh_SuperiorX', 'L_Thigh_SuperiorY', 'L_Thigh_SuperiorZ', 'L_Thigh_InferiorX', 'L_Thigh_InferiorY', 'L_Thigh_InferiorZ', 'L_Thigh_LateralX', 'L_Thigh_LateralY', 'L_Thigh_LateralZ'})
+lts = lts0{lts0{:,'Frame'} == 98, :}
 lt_sup = lts(2:4)
 lt_inf = lts(5:7)
 lt_lat = lts(8:end)
 
+si = lt_inf - lt_sup
+sl = lt_lat - lt_sup
+
+y_hat = -si./norm(si)
+
+n = cross(si, sl);
+x_hat = n./norm(n)
+z_hat = cross(x_hat,y_hat)
+
+rot_mat = [x_hat', y_hat', z_hat'];
+rot_mat(4,:) = 0;
+plts = [lt_sup';1];
+t_mat = [rot_mat, plts]
+%% 3iii)
+lts80 = lts0{lts0{:,'Frame'} == 80, :}
+
+lt_sup80 = lts80(2:4)
+lt_inf80 = lts80(5:7)
+lt_lat80 = lts80(8:end)
+
+si80 = lt_inf80 - lt_sup80
+sl80 = lt_lat80 - lt_sup80
+
+y_hat80 = -si80./norm(si80)
+
+n80 = cross(si80, sl80);
+x_hat80 = n80./norm(n80)
+z_hat80 = cross(x_hat80,y_hat80)
+
+rot_mat80 = [x_hat80', y_hat80', z_hat80'];
+rot_mat80(4,:) = 0;
+plts80 = [lt_sup80';1];
+t_mat80 = [rot_mat80, plts80];
+
+lkm = data(:,{'Frame','L_Knee_MedialX','L_Knee_MedialY', 'L_Knee_MedialZ'});
+lkm80 = lkm{lkm{:,'Frame'} == 80, :};
+lkm80 = [lkm80(2:end), 1]'
+lts_p_lkm = inv(t_mat80) * lkm80
+
+lab_p_lkm = t_mat*lts_p_lkm
