1cb67e68ef54249e955b3876c06a39e239d88a4e
James <jameszcliu@gmail.com>
2022-11-09 14:48:18 +0000

    Q4 Completed

--- coursework1.m
+++ coursework1.m
@@ -1,5 +1,6 @@
 clc;
 data = readtable("Marker_data_2022.xlsx");
+format longg;
 %% Q3)
 L_Knee_MedialX = data{22:87,'L_Knee_MedialX'};
 L_Knee_MedialY = data{22:87,'L_Knee_MedialY'};
@@ -50,26 +51,7 @@ scatter(t(TZF),ZF(TZF))
 lgd = legend("X Coordinate Raw", "X Coordinate Interpolated", "Y Coordinate Raw", "Y Coordinate Interpolated", "Z Coordinate Raw", "Z Coordinate Interpolated");
 lgd.Location = 'southwest';
 
-%% 3iii) Redundant Marker Filling
 
-
-
-figure(2); clf; hold on;
-title('Spline Interpolation of Left Medial Knee kinematics')
-
-% Plot x trajectory
-scatter(t, L_Knee_MedialX)
-scatter(t(TXF),XF(TXF))
-
-% Plot y trajectory
-scatter(t, L_Knee_MedialY)
-scatter(t(TYF),YF(TYF))
-
-% Plot z trajectory
-scatter(t, L_Knee_MedialZ)
-scatter(t(TZF),ZF(TZF))
-lgd = legend("X Coordinate Raw", "X Coordinate Interpolated", "Y Coordinate Raw", "Y Coordinate Interpolated", "Z Coordinate Raw", "Z Coordinate Interpolated");
-lgd.Location = 'southwest';
 
 %% 3 Transformation Matrix for Left Thigh Superior
 % left_thigh_sup_x = data{data{:,'Frame'} == 98, "L_Thigh_SuperiorX"}
@@ -98,7 +80,7 @@ rot_mat = [x_hat', y_hat', z_hat'];
 rot_mat(4,:) = 0;
 plts = [lt_sup';1];
 t_mat = [rot_mat, plts]
-%% 3iii)
+%% 3iii) Redundant Marker Filling
 lts80 = lts0{lts0{:,'Frame'} == 80, :}
 
 lt_sup80 = lts80(2:4)
@@ -131,9 +113,95 @@ clc;
 lab_lkm_vecs = [];
 tmats = []
 k = 1;
-for frame=78:123
+for frame=98:123
     lab_tmat_lts_i = get_lab_tmat_lts_i(frame, data);
     tmats(:,:,k) = lab_tmat_lts_i;
     lab_lkm_vecs(:,k) = lab_tmat_lts_i * lts_p_lkm80;
     k = k+1;
 end
+
+%% Plotting 3iii)
+figure(3); clf; hold on;
+title('Redundant Marker Filling of Left Medial Knee kinematics')
+
+% Plot x trajectory
+scatter(t, L_Knee_MedialX)
+scatter(t(TXF),lab_lkm_vecs(1,:))
+
+% Plot y trajectory
+scatter(t, L_Knee_MedialY)
+scatter(t(TYF),lab_lkm_vecs(2,:))
+
+% Plot z trajectory
+scatter(t, L_Knee_MedialZ)
+scatter(t(TZF),lab_lkm_vecs(3,:))
+lgd = legend("X Coordinate Raw", "X Coordinate Interpolated", "Y Coordinate Raw", "Y Coordinate Interpolated", "Z Coordinate Raw", "Z Coordinate Interpolated");
+lgd.Location = 'southwest';
+figure(3);
+
+%% Q4i) Right Thigh to Lab
+clc;
+format longg;
+% lab_rt_rs = [221.6410	371.8530	826.8640	223.3180	338.0770	588.5920	233.6706	339.0758	526.9028	215.0853	196.9434	528.2230];
+lab_rt_rs = data(:,{'Frame', 'R_Thigh_SuperiorX', 'R_Thigh_SuperiorY', 'R_Thigh_SuperiorZ', 'R_Thigh_InferiorX', 'R_Thigh_InferiorY', 'R_Thigh_InferiorZ', 'R_Knee_LateralX', 'R_Knee_LateralY', 'R_Knee_LateralZ', 'R_Knee_MedialX', 'R_Knee_MedialY', 'R_Knee_MedialZ'})
+lab_rt_rs_105 = lab_rt_rs{lab_rt_rs{:,'Frame'} == 105, :}
+rts = lab_rt_rs_105(2:4)'
+rti = lab_rt_rs_105(5:7)'
+rkl = lab_rt_rs_105(8:10)'
+rkm = lab_rt_rs_105(11:end)'
+
+v2 = rkm-rkl
+norm(v2)
+x_hat_105 = v2./norm(v2)
+
+% rt = data(:,{'Frame','R_Thigh_SuperiorX', 'R_Thigh_SuperiorY', 'R_Thigh_SuperiorZ', 'R_Thigh_InferiorX', 'R_Thigh_InferiorY', 'R_Thigh_InferiorZ'})
+% rts_105 = rt{rt{:,'Frame'} == 105,:}
+% 
+% rts = rts_105(2:4)'
+% rti = rts_105(5:end)'
+
+v1 = rts-rti
+
+n_105 = cross(v2,v1)
+mag_n = norm(n_105)
+z_hat_105 = n_105./norm(n_105)
+
+y_hat_105 = cross(z_hat_105,x_hat_105)
+
+patella = data(:, {'Frame', 'R_PatellaX', 'R_PatellaY', 'R_PatellaZ'})
+patella105 = patella{patella{:,'Frame'} == 105, :}
+patella105 = patella105(2:end)'
+lab_thigh_tmat_105 = [x_hat_105, y_hat_105, z_hat_105, patella105]
+lab_thigh_tmat_105 = [lab_thigh_tmat_105; 0 0 0 1]
+
+%% 4ii) Right Shank
+clc;
+format longg;
+% lab_rt_rs = [221.6410	371.8530	826.8640	223.3180	338.0770	588.5920	233.6706	339.0758	526.9028	215.0853	196.9434	528.2230];
+lab_rs_ra = data(:,{'Frame', 'R_Shank_SuperiorX', 'R_Shank_SuperiorY', 'R_Shank_SuperiorZ', 'R_Shank_InferiorX','R_Shank_InferiorY','R_Shank_InferiorZ', 'R_Ankle_LateralX','R_Ankle_LateralY','R_Ankle_LateralZ', 'RankleMedialX','RankleMedialY', 'RankleMedialZ'})
+lab_rs_ra_105 = lab_rs_ra{lab_rs_ra{:,'Frame'} == 105, :}
+rss = lab_rs_ra_105(2:4)'
+rsi = lab_rs_ra_105(5:7)'
+ral = lab_rs_ra_105(8:10)'
+ram = lab_rs_ra_105(11:end)'
+
+v2 = ram-ral
+norm(v2)
+x_hat_105 = v2./norm(v2)
+
+% rt = data(:,{'Frame','R_Thigh_SuperiorX', 'R_Thigh_SuperiorY', 'R_Thigh_SuperiorZ', 'R_Thigh_InferiorX', 'R_Thigh_InferiorY', 'R_Thigh_InferiorZ'})
+% rts_105 = rt{rt{:,'Frame'} == 105,:}
+% 
+% rts = rts_105(2:4)'
+% rti = rts_105(5:end)'
+
+v1 = rss-rsi
+
+n_105 = cross(v2,v1)
+mag_n = norm(n_105)
+z_hat_105 = n_105./norm(n_105)
+
+y_hat_105 = cross(z_hat_105,x_hat_105)
+
+lab_shank_tmat_105 = [x_hat_105, y_hat_105, z_hat_105, rsi]
+lab_shank_tmat_105 = [lab_shank_tmat_105; 0 0 0 1]
